bf4675c1ea036d2efca23dbf529f411b72a02ee5
brain <mykshada@daum.net>
2021-11-03 14:38:36 +0900

    commit

---
+++ DemoGradle10/src/main/java/com/example/demo/DemoGradle10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradle10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradle10Application.class, args);
+	}
+
+}
